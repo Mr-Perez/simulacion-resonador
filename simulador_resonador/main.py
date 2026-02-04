ff93d85c33610a2b5ac1403208ebeae920778fc4
Valentin Perez <valentinignaperez@gmail.com>
2026-02-04 11:27:24 -0300

    main.py

--- simulador_resonador/main.py
+++ simulador_resonador/main.py
@@ -1,1 +1,10 @@
+# simulador_resonador/main.py
+from ui.app import ResonadorApp
+
+def main():
+    app = ResonadorApp()
+    app.mainloop()   # 🔥 esto mantiene viva la ventana
+
+if __name__ == "__main__":
+    main()
 
